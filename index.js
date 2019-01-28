5cbd38b1336fed16a9745b2e61625dad77025abe
Daniel Ferro <dlcerva@gmail.com>
2019-01-27 21:11:36 -0500

    Updated configs

--- index.js
+++ index.js
@@ -1,102 +1,29 @@
 module.exports = {
   extends: [
     'eslint-config-airbnb-base',
-    'plugin:prettier/recommended'
+    'prettier'
   ],
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
   plugins: [
-    'prettier',
-    'html',
-    'jsdoc',
-    'json',
-    'node',
-    'prefer-object-spread'
+    'import',
+    'prettier'
   ],
-  globals: {
-    filterXSS: true,
-    document: true,
-    navigator: true,
-    window: true,
-    FileReader: true,
-    alert: true,
-    jQuery: true,
-    Blob: true,
-    $: true,
-    XMLHttpRequest: true,
-    contrast: true,
-    angular: true,
-    sessionStorage: true,
-    localStorage: true,
-    location: true,
-    CKEDITOR: true,
-    MediumEditor: true,
-    MediumButton: true,
-    moment: true,
-    MutationObserver: true,
-    Bloodhound: true,
-    Mustache: true,
-    Handlebars: true,
-    FormData: true,
-    _etmc: true,
-    ga: true,
-    Prism: true,
-    Vivus: true
-  },
-  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 7,
-    sourceType: 'module',
-    ecmaFeatures: {
-      classes: true,
-      experimentalObjectRestSpread: true,
-      impliedStrict: true,
-      jsx: true,
-      modules: true
-    }
+    ecmaVersion: 2017
+  },
+  env: {
+    node: true,
+    es6: true,
+    jest: true
   },
   rules: {
-    'arrow-body-style': 0,
-    'comma-dangle': ['error', 'never'],
-    'consistent-return': 0,
-    'func-names': 0,
-    'global-require': 0,
-    'import/no-named-as-default-member': 0,
-    'jsdoc/check-param-names': 1,
-    'jsdoc/check-tag-names': 1,
-    'jsdoc/check-types': 1,
-    'jsdoc/newline-after-description': 1,
-    'jsdoc/require-description-complete-sentence': 0,
-    'jsdoc/require-example': 0,
-    'jsdoc/require-hyphen-before-param-description': 1,
-    'jsdoc/require-param': 1,
-    'jsdoc/require-param-description': 1,
-    'jsdoc/require-param-name': 1,
-    'jsdoc/require-param-type': 1,
-    'jsdoc/require-returns-description': 1,
-    'jsdoc/require-returns-type': 1,
-    'node/no-deprecated-api': 'error',
-    'node/process-exit-as-throw': 'error',
-    'no-await-in-loop': 0,
-    'no-console': 0,
-    'no-loop-func': 0,
-    'no-nested-ternary': 0,
-    'no-param-reassign': ['error', { props: false }],
-    'no-script-url': 0,
-    'no-underscore-dangle': 0,
-    'no-unused-vars': ['error', { args: 'none' }],
-    'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
-    radix: 0,
-    'space-before-function-paren': ['error', {
-      anonymous: 'ignore',
-      named: 'ignore',
-      asyncArrow: 'always'
-    }]
+    'prettier/prettier': [
+      'error'
+    ]
   },
   settings: {
-    'html/html-extensions': ['.html', '.ejs']
+    'html/html-extensions': [
+      '.html',
+      '.ejs'
+    ]
   }
 };
