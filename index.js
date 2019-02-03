e103daea941fc7342a19f359f95491a9ee414dfa
Daniel Ferro <dlcerva@gmail.com>
2019-02-02 21:23:48 -0500

    Updated deps

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 module.exports = {
-  extends: [
-    'eslint-config-airbnb-base',
-    'prettier'
-  ],
+  extends: ['eslint-config-airbnb-base', 'plugin:node/recommended'],
   plugins: [
     'import',
-    'prettier'
+    'html',
+    'jsdoc',
+    'json',
+    'prefer-object-spread',
+    'promise'
   ],
   parserOptions: {
     ecmaVersion: 2017
@@ -15,15 +16,36 @@ module.exports = {
     es6: true,
     jest: true
   },
-  rules: {
-    'prettier/prettier': [
-      'error'
-    ]
-  },
   settings: {
-    'html/html-extensions': [
-      '.html',
-      '.ejs'
-    ]
+    'html/html-extensions': ['.html', '.ejs']
+  },
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'import/extensions': ['off', 'always', {
+      js: 'never',
+      vue: 'never',
+      jsx: 'never'
+    }],
+    'jsdoc/check-param-names': 1,
+    'jsdoc/check-tag-names': 1,
+    'jsdoc/check-types': 1,
+    'jsdoc/newline-after-description': 1,
+    'jsdoc/require-description-complete-sentence': 0,
+    'jsdoc/require-example': 0,
+    'jsdoc/require-hyphen-before-param-description': 1,
+    'jsdoc/require-param': 1,
+    'jsdoc/require-param-description': 1,
+    'jsdoc/require-param-name': 1,
+    'jsdoc/require-param-type': 1,
+    'jsdoc/require-returns-description': 1,
+    'jsdoc/require-returns-type': 1,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-object-spread/prefer-object-spread': 2,
+    'space-before-function-paren': ['error', {
+      anonymous: 'ignore',
+      named: 'ignore',
+      asyncArrow: 'always'
+    }]
   }
 };
