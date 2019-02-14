5dac53f112314cb2723c1a7abcc1c5df02ffd6b8
Daniel Ferro <dlcerva@gmail.com>
2019-02-14 11:19:59 -0500

    Readded prettier.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 module.exports = {
-  extends: ['eslint-config-airbnb-base', 'plugin:node/recommended'],
+  extends: ['eslint-config-airbnb-base', 'prettier', 'plugin:node/recommended'],
   plugins: [
     'import',
     'html',
     'jsdoc',
     'json',
     'prefer-object-spread',
+    'prettier',
     'promise'
   ],
   parserOptions: {
@@ -20,12 +21,17 @@ module.exports = {
     'html/html-extensions': ['.html', '.ejs']
   },
   rules: {
+    'prettier/prettier': ['error'],
     'comma-dangle': ['error', 'never'],
-    'import/extensions': ['off', 'always', {
-      js: 'never',
-      vue: 'never',
-      jsx: 'never'
-    }],
+    'import/extensions': [
+      'off',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+        jsx: 'never'
+      }
+    ],
     'jsdoc/check-param-names': 1,
     'jsdoc/check-tag-names': 1,
     'jsdoc/check-types': 1,
@@ -42,10 +48,13 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'prefer-object-spread/prefer-object-spread': 2,
-    'space-before-function-paren': ['error', {
-      anonymous: 'ignore',
-      named: 'ignore',
-      asyncArrow: 'always'
-    }]
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'ignore',
+        named: 'ignore',
+        asyncArrow: 'always'
+      }
+    ]
   }
 };
